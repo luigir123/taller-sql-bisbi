f2bdad9f6e32d1cc1e5ecb7275f269d52412e902
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 20:35:50 -0500

    Refactor SQL query with comments
    Added comments and formatted SQL query for clarity.

--- 07_ejercicio.sql
+++ 07_ejercicio.sql
@@ -1,1 +1,12 @@
-SELECT nombre, precio FROM productos WHERE categoria = 'Accesorios' AND precio < 500;
+--EJERCICIO 7
+SELECT nombre, precio
+FROM productos
+WHERE categoria = 'Accesorios'
+AND precio < 500;
+--RESPUESTA:
+--nombre                   	        precio
+--"Casco Abierto Ligero"	           "450"
+--"Altímetro Digital Precisión"	     "280"
+--"Altímetro Analógico Confiable"	   "150"
+--"Gafas Panorámicas Anti-reflejo"	 "320"
+--"Guantes Térmicos Profesionales"	 "240"
