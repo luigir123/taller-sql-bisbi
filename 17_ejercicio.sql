18f150ec3a3e771bb84075564bb06c9e1bfdb76a
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 21:01:21 -0500

    Update 17_ejercicio.sql

--- 17_ejercicio.sql
+++ 17_ejercicio.sql
@@ -1,1 +1,6 @@
-SELECT SUM((total_venta / 0.70) - total_venta) AS dinero_no_ingresado FROM ventas WHERE cupon_usado = 'ULTIMO_SUSPIRO';
+-- EJERCICIO 17
+SELECT SUM((total_venta / 0.70) - total_venta) AS dinero_no_ingresado
+FROM ventas
+WHERE cupon_usado = 'ULTIMO_SUSPIRO';
+-- RESPUESTA:
+-- dinero_no_ingresado = 359385
