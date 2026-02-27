c47e5dd7d68d6b8b4b145565f9e2b64285792739
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 20:28:50 -0500

    Enhance SQL query with comments
    Added comments to clarify the SQL query.

--- 05_ejercicio.sql
+++ 05_ejercicio.sql
@@ -1,1 +1,6 @@
-SELECT nombre, correo, fecha_nacimiento FROM clientes ORDER BY nombre ASC;
+--EJERCICIO 5
+SELECT nombre, correo, fecha_nacimiento
+FROM clientes
+ORDER BY nombre ASC;
+--RESPUESTA:
+--Lista completa ordenada alfabéticamente.
