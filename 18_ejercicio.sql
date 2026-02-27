15f860d423153e690fdc94337bbc5a850bee4b77
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 21:02:56 -0500

    Update 18_ejercicio.sql

--- 18_ejercicio.sql
+++ 18_ejercicio.sql
@@ -1,1 +1,10 @@
-SELECT c.nombre, CAST((julianday('now') - julianday(c.fecha_nacimiento))/365 AS INTEGER) AS edad_aprox, SUM(v.total_venta) AS total_gastado FROM clientes c JOIN ventas v ON c.id_cliente = v.id_cliente GROUP BY c.id_cliente HAVING edad_aprox > 60;
+--EJERCICIO 18
+SELECT c.nombre,
+       CAST((julianday('now') - julianday(c.fecha_nacimiento)) / 365 AS INT) AS edad,
+       SUM(v.total_venta) AS total_gastado
+FROM clientes c
+JOIN ventas v ON c.id_cliente = v.id_cliente
+WHERE (julianday('now') - julianday(c.fecha_nacimiento)) / 365 > 60
+GROUP BY c.id_cliente;
+--RESPUESTA:
+--Lista de clientes senior compradores.
