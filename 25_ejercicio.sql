150703b09110ed05e919bbb36f4003c8942c44e1
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 21:32:34 -0500

    Improve SQL query formatting and add comments
    Refactor SQL query for clarity and add comments.

--- 25_ejercicio.sql
+++ 25_ejercicio.sql
@@ -1,1 +1,11 @@
-SELECT c.nombre, COUNT(DISTINCT dv.id_producto) AS variedad_productos FROM detalle_ventas dv JOIN ventas v ON dv.id_venta = v.id_venta JOIN clientes c ON v.id_cliente = c.id_cliente GROUP BY v.id_cliente ORDER BY variedad_productos DESC LIMIT 1;
+--EJERCICIO 25
+SELECT c.nombre,
+       COUNT(DISTINCT d.id_producto) AS variedad_productos
+FROM ventas v
+JOIN detalle_ventas d ON v.id_venta = d.id_venta
+JOIN clientes c ON v.id_cliente = c.id_cliente
+GROUP BY v.id_cliente
+ORDER BY variedad_productos DESC
+LIMIT 1;
+--RESPUESTA:
+--Cliente con mayor diversidad de compras.
