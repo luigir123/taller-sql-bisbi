914ec4c7f3ce495f4d371e94b97e9403fa4eb6fe
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 20:37:25 -0500

    Enhance SQL query with comments for clarity
    Added comments to clarify the SQL query and its expected result.

--- 08_ejercicio.sql
+++ 08_ejercicio.sql
@@ -1,1 +1,6 @@
-SELECT COUNT(*) AS ventas_con_cupon FROM ventas WHERE cupon_usado = 'ULTIMO_SUSPIRO';
+--EJERCICIO 8
+SELECT COUNT(*) AS ventas_con_cupon
+FROM ventas
+WHERE cupon_usado = 'ULTIMO_SUSPIRO';
+--RESPUESTA:
+--ventas_con_cupon = 250
