0fb2969ca717de38c06bcfb712ebe512aad03a6b
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 20:57:09 -0500

    Change query to find the cheapest accessory
    Updated query to select the cheapest accessory with details.

--- 15_ejercicio.sql
+++ 15_ejercicio.sql
@@ -1,1 +1,8 @@
-SELECT MIN(precio) AS accesorio_mas_barato FROM productos WHERE categoria = 'Accesorios';
+-- EJERCICIO 15
+SELECT nombre, precio
+FROM productos
+WHERE categoria = 'Accesorios'
+ORDER BY precio ASC
+LIMIT 1;
+-- RESPUESTA:
+-- Accesorio más barato: Altímetro Analógico Confiable, 150
