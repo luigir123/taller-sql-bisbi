352b0e311bd589c90cbc571f2b2e93a8f905816b
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 20:49:03 -0500

    Add comments to SQL query for exercise 12
    Added comments for exercise context and response.

--- 12_ejercicio.sql
+++ 12_ejercicio.sql
@@ -1,1 +1,5 @@
-SELECT AVG(precio) AS precio_promedio FROM productos;
+-- EJERCICIO 12
+SELECT AVG(precio) AS precio_promedio
+FROM productos;
+-- RESPUESTA:
+-- precio_promedio = 1591.3333333333333
