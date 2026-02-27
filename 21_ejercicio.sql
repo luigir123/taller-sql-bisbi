d2bdcf1ef49e04cc26183beac294cb913f85a723
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 21:24:34 -0500

    Enhance SQL query with comments
    Added comments to clarify the purpose of the SQL query.

--- 21_ejercicio.sql
+++ 21_ejercicio.sql
@@ -1,1 +1,10 @@
-SELECT c.nombre, COUNT(*) AS veces_usado FROM ventas v JOIN clientes c ON v.id_cliente = c.id_cliente WHERE v.cupon_usado = 'ULTIMO_SUSPIRO' GROUP BY v.id_cliente HAVING COUNT(*) >= 3;
+--EJERCICIO 21
+SELECT c.nombre,
+       COUNT(*) AS veces_usado
+FROM ventas v
+JOIN clientes c ON v.id_cliente = c.id_cliente
+WHERE v.cupon_usado = 'ULTIMO_SUSPIRO'
+GROUP BY v.id_cliente
+HAVING COUNT(*) >= 3;
+--RESPUESTA:
+--Lista de clientes frecuentes del cupón.
