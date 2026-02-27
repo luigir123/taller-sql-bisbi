6fa1432fe103790082ac625d56846e4d1fb1763c
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 21:37:06 -0500

    Refactor SQL query for ingresos_mensuales
    Refactor SQL query to improve readability and add comments.

--- 27_ejercicio.sql
+++ 27_ejercicio.sql
@@ -1,1 +1,12 @@
-WITH ingresos_mensuales AS ( SELECT strftime('%m', fecha_venta) AS mes, SUM(total_venta) AS total_mes FROM ventas WHERE strftime('%Y', fecha_venta) = '2025' GROUP BY mes ) SELECT MAX(total_mes) - MIN(total_mes) AS diferencia_ingresos FROM ingresos_mensuales;
+--EJERCICIO 27
+WITH ingresos_mensuales AS (
+    SELECT strftime('%m', fecha_venta) AS mes,
+           SUM(total_venta) AS ingresos
+    FROM ventas
+    WHERE strftime('%Y', fecha_venta) = '2025'
+    GROUP BY mes
+)
+SELECT MAX(ingresos) - MIN(ingresos) AS diferencia_ingresos
+FROM ingresos_mensuales;
+--RESPUESTA:
+--diferencia_ingresos = 98593
