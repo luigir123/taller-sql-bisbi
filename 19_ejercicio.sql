1126d255c4c562c6af49b3427ace20ba87e8a786
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 21:05:53 -0500

    Improve SQL query formatting and add comments
    Refactor SQL query for better readability and add comments.

--- 19_ejercicio.sql
+++ 19_ejercicio.sql
@@ -1,1 +1,10 @@
-SELECT p.nombre, SUM(dv.cantidad) AS total_unidades FROM detalle_ventas dv JOIN productos p ON dv.id_producto = p.id_producto GROUP BY dv.id_producto ORDER BY total_unidades DESC LIMIT 1;
+--EJERCICIO 19
+SELECT p.nombre,
+       SUM(d.cantidad) AS total_unidades
+FROM detalle_ventas d
+JOIN productos p ON d.id_producto = p.id_producto
+GROUP BY d.id_producto
+ORDER BY total_unidades DESC
+LIMIT 1;
+--RESPUESTA:
+--Producto más vendido: Gafas de Noche Infra-ventiladas, 274
