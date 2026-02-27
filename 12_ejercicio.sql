6142457a86757163afc57803d6c4217d0e0eb712
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 20:49:20 -0500

    Update comments in 12_ejercicio.sql

--- 12_ejercicio.sql
+++ 12_ejercicio.sql
@@ -1,5 +1,5 @@
--- EJERCICIO 12
+--EJERCICIO 12
 SELECT AVG(precio) AS precio_promedio
 FROM productos;
--- RESPUESTA:
--- precio_promedio = 1591.3333333333333
+--RESPUESTA:
+--precio_promedio = 1591.3333333333333
