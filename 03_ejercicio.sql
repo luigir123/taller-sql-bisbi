f1b2ed74a9cf82b48235b2e2a0194e416ce13f00
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 20:22:18 -0500

    Modify SQL query to select most expensive parachute
    Updated query to retrieve the most expensive parachute with additional details.

--- 03_ejercicio.sql
+++ 03_ejercicio.sql
@@ -1,1 +1,9 @@
-SELECT MAX(precio) AS paracaidas_mas_caro FROM productos WHERE categoria = 'Paracaídas';
+--EJERCICIO 3
+SELECT nombre, precio
+FROM productos
+WHERE categoria = 'Paracaídas'
+ORDER BY precio DESC
+LIMIT 1;
+--RESPUESTA:
+--Producto más caro: "Paracaídas Tandem para Instructor"	
+--Precio: 5200
