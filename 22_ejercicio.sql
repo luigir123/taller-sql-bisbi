8df641d4e85269b69829cea69e2eb9ef1718f5a1
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 21:25:51 -0500

    Improve SQL query formatting and add comments
    Refactor SQL query for better readability and add comments.

--- 22_ejercicio.sql
+++ 22_ejercicio.sql
@@ -1,1 +1,13 @@
-SELECT c.nombre, SUM(v.total_venta) AS total_gastado, CASE WHEN SUM(v.total_venta) > 5000 THEN 'Alto Valor' WHEN SUM(v.total_venta) BETWEEN 2000 AND 5000 THEN 'Medio Valor' ELSE 'Bajo Valor' END AS nivel_cliente FROM ventas v JOIN clientes c ON v.id_cliente = c.id_cliente GROUP BY v.id_cliente;
+--EJERCICIO 22
+SELECT c.nombre,
+       SUM(v.total_venta) AS total_gastado,
+       CASE
+           WHEN SUM(v.total_venta) > 5000 THEN 'Alto Valor'
+           WHEN SUM(v.total_venta) BETWEEN 2000 AND 5000 THEN 'Medio Valor'
+           ELSE 'Bajo Valor'
+       END AS categoria_cliente
+FROM ventas v
+JOIN clientes c ON v.id_cliente = c.id_cliente
+GROUP BY v.id_cliente;
+--RESPUESTA:
+--Se obtiene segmentación de clientes.
