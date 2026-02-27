31c1084383973e6ae2ea90a30b9762ce0a9b6c5c
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 21:40:01 -0500

    Update SQL query for coupon percentage calculation
    Refactor SQL query to calculate coupon percentage using CASE statement.

--- 29_ejercicio.sql
+++ 29_ejercicio.sql
@@ -1,1 +1,8 @@
-SELECT (SELECT SUM(total_venta) FROM ventas WHERE cupon_usado = 'ULTIMO_SUSPIRO') * 100.0 / (SELECT SUM(total_venta) FROM ventas) AS porcentaje_cupon;
+-- EJERCICIO 29
+SELECT 
+    (SUM(CASE WHEN cupon_usado = 'ULTIMO_SUSPIRO' 
+              THEN total_venta ELSE 0 END) 
+     / SUM(total_venta)) * 100 AS porcentaje_cupon
+FROM ventas;
+--RESPUESTA:
+--porcentaje_cupon = 15.030969283453862
