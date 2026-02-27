76d7f7d4b44e52ec3b2b573449c17ae88ac7110b
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 20:51:28 -0500

    Refactor SQL query with comments and formatting
    Added comments and formatted SQL query for clarity.

--- 13_ejercicio.sql
+++ 13_ejercicio.sql
@@ -1,1 +1,6 @@
-SELECT COUNT(*) AS total_paracaidas FROM productos WHERE categoria = 'Paracaídas';
+--EJERCICIO 13
+SELECT COUNT(*) AS total_paracaidas
+FROM productos
+WHERE categoria = 'Paracaídas';
+--RESPUESTA:
+--total_paracaidas = 5
