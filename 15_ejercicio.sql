e2d096619c1ff7c7bf37cc6444c026cf7fe828dc
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 21:03:25 -0500

    Update comments in 15_ejercicio.sql

--- 15_ejercicio.sql
+++ 15_ejercicio.sql
@@ -1,8 +1,8 @@
--- EJERCICIO 15
+--EJERCICIO 15
 SELECT nombre, precio
 FROM productos
 WHERE categoria = 'Accesorios'
 ORDER BY precio ASC
 LIMIT 1;
--- RESPUESTA:
--- Accesorio más barato: Altímetro Analógico Confiable, 150
+--RESPUESTA:
+--Accesorio más barato: Altímetro Analógico Confiable, 150
