35dcf7cec9c5b1523ade9d7abce2bccb9da62479
luigir123 <luigibisbicuzrequene@gmail.com>
2026-02-26 20:54:55 -0500

    Refactor SQL query with comments and formatting
    Added comments and formatted SQL query for clarity.

--- 14_ejercicio.sql
+++ 14_ejercicio.sql
@@ -1,1 +1,7 @@
-SELECT * FROM ventas WHERE strftime('%Y', fecha_venta) = '2025' ORDER BY fecha_venta DESC;
+--EJERCICIO 14
+SELECT *
+FROM ventas
+WHERE strftime('%Y', fecha_venta) = '2025'
+ORDER BY fecha_venta DESC;
+--RESPUESTA:
+--Se listan todas las ventas del año 2025.
